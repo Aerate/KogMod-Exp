f48211a7c71aa53a3f9ea5ad4eae21df3b6fb4eb
Rene K <renekollmann21@gmail.com>
2019-01-11 10:49:50 +0100

    ready 4 review

---
+++ Python/stimuli_gen.sh
@@ -0,0 +1,17 @@
+#For fast Stimuli Generation
+
+#Triangles
+python stimuluserzeuger.py -dl 5 -f "svg" -dir "./examples/triangle" -o "triangle" -nd 15
+
+#Diamonds
+python stimuluserzeuger.py -dl 5 -f "svg" -dir "./examples/diamond" -o "diamond" -nd 15
+
+#Ms
+python stimuluserzeuger.py -dl 5 -f "svg" -dir "./examples/M" -o "M" -nd 15
+
+#Fs
+python stimuluserzeuger.py -dl 5 -f "svg" -dir "./examples/F" -o "F" -nd 15
+
+#Randoms
+python stimuluserzeuger.py -dl 5 -f "svg" -dir "./examples/random" -o "random" -nd 15
+
--- Python/stimuluserzeuger.py
+++ Python/stimuluserzeuger.py
@@ -4,11 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-dl', '--distortion_level', type=int, default=4, help='Intensity Level of Distortion (1 - 9), Note: 7.7, 8.6, 9.7 are denoted as 7,8,9 respectively')
-parser.add_argument('-f', '--output_format', type=str, default="svg", help='Output Format, supported: svg or png')
-parser.add_argument('-dir', '--output_dir', type=str, default="./output", help='Output Location of Stimuli Images')
-parser.add_argument('-o', '--original', type=str, default="triangle", help='Original Form, supported: triangle, diamond, M, F, rand')
-parser.add_argument('-nd', '--num_distortions', type=int, default=10, help='Number of Distortion Images to create (default 10)')
+parser.add_argument('-dl', '--distortion_level', type=int, default=4,
+                    help='Intensity Level of Distortion (1 - 9), Note: 7.7, 8.6, 9.7 are denoted as 7,8,9 respectively')
+parser.add_argument('-f', '--output_format', type=str, default="svg",
+                    help='Output Format, supported: svg or png')
+parser.add_argument('-dir', '--output_dir', type=str, default="./examples",
+                    help='Output Location of Stimuli Images')
+parser.add_argument('-o', '--original', type=str, default="triangle",
+                    help='Original Form, supported: triangle, diamond, M, F, rand')
+parser.add_argument('-nd', '--num_distortions', type=int, default=10,
+                    help='Number of Distortion Images to create (default 10)')
 FLAGS = parser.parse_args()
 
 origin_dimension = 30
@@ -23,7 +28,8 @@ def create_stimuli():
 
     for i in range(0, FLAGS.num_distortions):
         distorted_points_x, distorted_points_y = distort_points(origin_points_x, origin_points_y)
-        plot_stimuli(distorted_points_x, distorted_points_y, FLAGS.original + "_distorted_l" + str(FLAGS.distortion_level)+ "_" + str(i))
+        plot_stimuli(distorted_points_x, distorted_points_y, FLAGS.original + "_distorted_l" +
+                     str(FLAGS.distortion_level)+ "_" + str(i))
 
 
 def create_origin_points():
@@ -33,6 +39,14 @@ def create_origin_points():
     """
     if FLAGS.original == "triangle":
         return np.array([-9, -3, 3, 9, -6, 6, -3, 3, 0]), np.array([-9, -9, -9, -9, -3, -3, 3, 3, 9])
+    if FLAGS.original == "diamond":
+        return np.array([0, -5, 5, -10, 10, -5, 5, 0]), np.array([-10, -5, -5, 0, 0, 5, 5, 10])
+    if FLAGS.original == "M":
+        return np.array([-8, -8, -8, -4, 0, 4, 8, 8, 8]), np.array([-8, 0, 8, 4, 0, 4, 8, 0, -8])
+    if FLAGS.original == "F":
+        return np.array([-6, -6, -6, -6, -2, -2, 2, 2, 6]), np.array([-6, -2, 2, 6, 2, 6, 2, 6, 6])
+    if FLAGS.original == "random":
+        return np.random.randint(-15, 16, size=10), np.random.randint(-15, 16, size=10)
 
 
 def plot_stimuli(x, y, name):
@@ -40,42 +54,72 @@ def plot_stimuli(x, y, name):
 
     :param x:
     :param y:
+    :param name:
     :return:
     """
     if not os.path.exists(FLAGS.output_dir):
         os.makedirs(FLAGS.output_dir)
 
+    x_corner = np.array([-25, -25, 25, 25])
+    y_corner = np.array([-25, 25, -25, 25])
+
+    axes = plt.gca()
+    axes.set_xlim([-25, 25])
+    axes.set_ylim([-25, 25])
     plt.axis("equal")
     plt.axis("off")
-    plt.scatter(x, y)
+    plt.xticks([])
+    plt.yticks([])
+    plt.scatter(x, y, color='black')
+    plt.scatter(x_corner, y_corner, color='white')
     plt.savefig(os.path.join(FLAGS.output_dir, name + "." + FLAGS.output_format))
-
     plt.gcf().clear()
 
 
 def distort_points(xs, ys):
     """
 
-    :param x:
-    :param y:
+    :param xs:
+    :param ys:
     :return:
     """
     distorted_xs = np.copy(xs)
     distorted_ys = np.copy(ys)
 
     for i in range(0, len(xs)):
-        distortion_area = get_distortion_area(FLAGS.distortion_level - 1)
-        distortion_offset_x, distortion_offset_y = get_distortion_offsets(distortion_area)
+        distortion_offset_x, distortion_offset_y = get_distortion_offsets(FLAGS.distortion_level)
         distorted_xs[i] += distortion_offset_x
         distorted_ys[i] += distortion_offset_y
 
     return distorted_xs, distorted_ys
 
 
+def get_distortion_offsets(distortion_level):
+    """
+
+    :param distortion_level:
+    :return:
+    """
+
+    if distortion_level <= 7:
+        distortion_area = get_distortion_area(distortion_level)
+
+        if distortion_area == 0:
+            return 0, 0
+        elif distortion_area == 1:
+            return generate_offset(0, 1)
+        elif distortion_area == 2:
+            return generate_offset(1, 2)
+        elif distortion_area == 3:
+            return generate_offset(2, 10)
+        elif distortion_area == 4:
+            return generate_offset(10, 20)
+
+
 def get_distortion_area(distortion_level):
     """
 
-    :param distorion_level:
+    :param distortion_level:
     :return:
     """
     #Probability Table after
@@ -94,26 +138,6 @@ def get_distortion_area(distortion_level):
         if rand < counter:
             return i
 
-
-def get_distortion_offsets(distortion_area):
-    """
-
-    :param distortion_point:
-    :return:
-    """
-
-    if distortion_area == 0:
-        return 0,0
-    elif distortion_area == 1:
-        return generate_offset(0, 1)
-    elif distortion_area == 2:
-        return generate_offset(1, 2)
-    elif distortion_area == 3:
-        return generate_offset(2, 5)
-    elif distortion_area == 4:
-        return generate_offset(5,10)
-    elif distortion_area == 5:
-        return generate_offset(10,20)
 
 def generate_offset(inner_bound, outer_bound):
     """
@@ -139,4 +163,6 @@ def generate_offset(inner_bound, outer_bound):
             return x_offset, y_offset
 
 
+#Execute main
 create_stimuli()
+print("Successfully created Stimuli: " + FLAGS.original)
